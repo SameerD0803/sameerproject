25fa25949972358906ce70c11669fd033902f43a
SameerD0803 <143246011+SameerD0803@users.noreply.github.com>
2023-10-01 12:50:09 +0000

    Datatypes

---
+++ 01.basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all Js code as newer version
+
+let name = "Sameer";
+let id = "18";
+let isLoggedInn= false;
+
+/*
+Primitive Data Types
+    Number => 2 to power 53
+    bigint
+    String => ""
+    boolean => true/false
+    null => standalone value
+    undefined => 
+    symbol => unique
+Non-Primitive 
+    object
+*/
+
+console.log(typeof "Sameer");
