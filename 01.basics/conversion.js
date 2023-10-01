33084f4007899e6e56d922ec2247fc6b202b3951
SameerD0803 <143246011+SameerD0803@users.noreply.github.com>
2023-10-01 13:28:36 +0000

    Conversion

---
+++ 01.basics/conversion.js
@@ -0,0 +1,45 @@
+/*let score = "33";
+
+console.log(typeof score);// string 
+console.log(typeof (score));// string
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);// number
+console.log(valueInNumber);// 33
+*/
+
+/*
+let score = true;
+console.log(typeof score);// boolean
+console.log(typeof (score));// boolean
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);// number
+console.log(valueInNumber);// 1 
+*/
+
+/*
+let score = 33;
+console.log(typeof score); // number
+let valueString = String(score);
+console.log(typeof valueString);// string
+console.log(valueString);// 33
+*/
+
+/*
+let score = "33abc";
+console.log(typeof score);// string 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);// number
+console.log(valueInNumber);// NaN
+*/
+
+
+
+/*  Note
+    "33" => 33
+    "33abc" => NaN
+    true => 1; false =>;
+    "Sameer" => true; "" => false;
+*/
+
+
+
